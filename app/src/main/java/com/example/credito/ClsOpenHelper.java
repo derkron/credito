8db744f615e628c3335610d368799fe94fc382b0
@TREBOL3094 <morales.esteban47@gmail.com>
2022-10-31 16:25:15 -0500

    Se crea aplicacion android de credito

---
+++ app/src/main/java/com/example/credito/ClsOpenHelper.java
@@ -0,0 +1,22 @@
+package com.example.credito;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class ClsOpenHelper extends SQLiteOpenHelper {
+    public ClsOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
